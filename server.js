8ee02ce6e75c7d3936a6b6f68e250a5ef57cd0ad
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-09-24 21:49:32 -0300

    fixed and added: crud

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
+app.use(express.json()); //Permitir o envio de dados via api rests
 
 //Iniciando a DB
 mongoose.connect(
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,20 +3,39 @@ const Product = mongoose.model('Product');
 
 
 module.exports = {
-
-    async store(req, res){
-        //Criação
-        const product = await Product.create(req.body);
-
-        return res.json(product);
-    },
-
+    //SELECT
     async index(req, res){
         const products = await Product.find();
 
         return res.json(products);
     },
 
+    //SELECT DETALHADO
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
 
+        return res.json(product);
+    },
+
+    //CREATE
+    async store(req, res){
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    },
+
+    //UPDATE
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true }); //Acha o produto e atualiza com a req do body
+
+        return res.json(product);
+    },
+
+    //DELETE
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+    },
 };
 
